1e511d16a18c7e6ea7d6e091c3981095a59d0b22
Peter Nguyen <peternguyenforwork@gmail.com>
2023-02-11 18:38:34 -0800

    added react-draggable to make each task draggable

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react"
 import classNames from "classnames"
+import Draggable from "react-draggable"
 
 import { Checkbox } from "@/components/ui/checkbox"
 
@@ -24,30 +25,49 @@ function Task({ task, taskId, setTasks }) {
   const [isTaskCompleted, setIsTaskCompleted] = useState<Boolean>(false)
 
   return (
-    <li className="task flex items-center space-x-2 rounded-lg border-b border-l px-2 py-3">
-      <Checkbox
-        className="rounded-full text-lg text-green-500 dark:text-white"
-        onClick={() =>
-          setIsTaskCompleted((prevIsTaskCompleted) => !prevIsTaskCompleted)
-        }
-      />
-      <label className="leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-        <input
-          className={classNames(
-            "bg-inherit p-0 focus:border-0 focus:outline-0 focus:ring-0",
-            { "line-through": isTaskCompleted }
-          )}
-          type="text"
-          value={task}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            event.target.value !== "" &&
-            setTasks((prevTasks: { [id: string]: String }) => ({
-              ...prevTasks,
-              [taskId]: event.target.value,
-            }))
+    <Draggable axis="y">
+      <li className="task flex items-center space-x-2 rounded-lg border-b border-l px-2 py-3">
+        <Checkbox
+          className="rounded-full text-lg text-green-500 dark:text-white"
+          onClick={() =>
+            setIsTaskCompleted((prevIsTaskCompleted) => !prevIsTaskCompleted)
           }
         />
-      </label>
-    </li>
+        <label className="leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+          <input
+            className={classNames(
+              "bg-inherit p-0 focus:border-0 focus:outline-0 focus:ring-0",
+              { "line-through": isTaskCompleted }
+            )}
+            type="text"
+            value={task}
+            onClick={(event: React.MouseEvent<HTMLInputElement>) =>
+              event.stopPropagation()
+            }
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              event.target.value !== "" &&
+              setTasks((prevTasks: { [id: string]: String }) => ({
+                ...prevTasks,
+                [taskId]: event.target.value,
+              }))
+            }
+          />
+        </label>
+      </li>
+    </Draggable>
   )
+}
+
+class Point {
+  x: number
+  y: number
+
+  constructor(x: number, y: number) {
+    this.x = x
+    this.y = y
+  }
+
+  equal(point: Point): boolean {
+    return this.x === point.x && this.y === point.y
+  }
 }
